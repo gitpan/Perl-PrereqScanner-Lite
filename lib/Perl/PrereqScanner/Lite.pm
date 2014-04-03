70bfcefc9807d663a3f6905ac480935ed4dfd566
Taiki Kawakami <moznion@gmail.com>
2014-04-03 13:19:59 +0000

    Import of MOZNION/Perl-PrereqScanner-Lite-0.17 from CPAN.
    gitpan-cpan-distribution: Perl-PrereqScanner-Lite
    gitpan-cpan-version:      0.17
    gitpan-cpan-path:         MOZNION/Perl-PrereqScanner-Lite-0.17.tar.gz
    gitpan-cpan-author:       MOZNION
    gitpan-cpan-maturity:     released

--- lib/Perl/PrereqScanner/Lite.pm
+++ lib/Perl/PrereqScanner/Lite.pm
@@ -6,7 +6,7 @@ use Compiler::Lexer;
 use CPAN::Meta::Requirements;
 use Perl::PrereqScanner::Lite::Constants;
 
-our $VERSION = "0.16";
+our $VERSION = "0.17";
 
 sub new {
     my ($class, $opt) = @_;
@@ -156,7 +156,7 @@ sub _scan {
             }
 
             # End of declare of use statement
-            if ($token_type == SEMI_COLON || $token->{data} eq ';') {
+            if ($token_type == SEMI_COLON || $token->{data} eq ';' || $token_type == LEFT_BRACE || $token_type == LEFT_BRACKET) {
                 #                            ~~~~~~~~~~~~~~~~~~~~~ XXX This problem fixed at https://github.com/goccy/p5-Compiler-Lexer/commit/248c63b6ebf8234657f775e8bd76760af3d00134
                 #                                                  But it has not released yet (in 2014-03-23).
                 if ($module_name && !$does_use_lib_or_constant) {
@@ -382,6 +382,20 @@ For example
     use Bar; ## no prereq
 
 In this case C<Foo> is the prereq, however C<Bar> is ignored.
+
+=head1 SPEED COMPARISON
+
+=head2 Plain
+
+                                Rate   Perl::PrereqScanner Perl::PrereqScanner::Lite
+    Perl::PrereqScanner       8.57/s                    --                      -97%
+    Perl::PrereqScanner::Lite  246/s                 2770%                        --
+
+=head2 With Moose scanner
+
+                                Rate   Perl::PrereqScanner Perl::PrereqScanner::Lite
+    Perl::PrereqScanner       9.00/s                    --                      -94%
+    Perl::PrereqScanner::Lite  152/s                 1587%                        --
 
 =head1 SEE ALSO
 
